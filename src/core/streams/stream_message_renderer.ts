22d44fe972590410630f2866c9c1c9eac03ae813
Sean Doyle <sean.p.doyle24@gmail.com>
2022-08-16 11:29:15 -0400

    Turbo Streams: Manage element focus
    When a `<turbo-stream>` modifies the document, it has the potential to
    affect which element has focus.
    
    For example, consider an element with an `[id]` that has focus:
    
    ```html
    <label for="an-input">
    <input id="an-input" value="an invalid value"> <!-- this element has focus -->
    ```
    
    Next, consider a `<turbo-stream>` element to replace it:
    
    ```html
    <turbo-stream action="replace" target="an-input">
      <template>
        <input id="an-input" value="an invalid value" class="invalid-input">
      </template>
    </turbo-stream>
    ```
    
    Prior to this commit, rendering that `<turbo-stream>` would remove the
    element with focus, and never restore it.
    
    After this commit, the `Session` will capture the `[id]` value of the
    element with focus (if there is any), then "restore" focus to an element
    in the document with a matching `[id]` attribute _after_ the render.
    
    Similarly, consider a `<turbo-stream>` that appends an element with
    `[autofocus]`:
    
    ```html
    <turbo-stream action="append" targets="body">
      <template>
        <input autofocus>
      </template>
    </turbo-stream>
    ```
    
    Prior to this commit, inserting an `[autofocus]` into the document with
    a `<turbo-stream>` had no effect.
    
    After this commit, the `Session` will scan any `<turbo-stream>` elements
    its about to render, extracting the first focusable element that
    declares an `[autofocus]` attribute.
    
    Once the rendering is complete, it will attempt to autofocus that
    element. Several scenarios will prevent that, including:
    
    * there aren't any `[autofocus]` elements in the collection of
      `<turbo-stream>` elements
    * the `[autofocus]` element does not exist in the document after the
      rendering is complete
    * the document already has an element with focus

--- src/core/streams/stream_message_renderer.ts
+++ src/core/streams/stream_message_renderer.ts
@@ -2,13 +2,20 @@ import { StreamMessage } from "./stream_message"
 import { StreamElement } from "../../elements/stream_element"
 import { Bardo, BardoDelegate } from "../bardo"
 import { PermanentElementMap, getPermanentElementById, queryPermanentElementsAll } from "../snapshot"
+import { around, elementIsFocusable, nextAnimationFrame, queryAutofocusableElement, uuid } from "../../util"
 
 export class StreamMessageRenderer implements BardoDelegate {
   render({ fragment }: StreamMessage) {
-    Bardo.preservingPermanentElements(this, getPermanentElementMapForFragment(fragment), () =>
-      document.documentElement.appendChild(fragment),
-    )
+    Bardo.preservingPermanentElements(this, getPermanentElementMapForFragment(fragment), () => {
+      withAutofocusFromFragment(fragment, () => {
+        withPreservedFocus(() => {
+          document.documentElement.appendChild(fragment)
+        })
+      })
+    })
   }
+
+  // Bardo delegate
 
   enteringBardo(currentPermanentElement: Element, newPermanentElement: Element) {
     newPermanentElement.replaceWith(currentPermanentElement.cloneNode(true))
@@ -33,4 +40,61 @@ function getPermanentElementMapForFragment(fragment: DocumentFragment): Permanen
   }
 
   return permanentElementMap
+}
+
+async function withAutofocusFromFragment(fragment: DocumentFragment, callback: () => void) {
+  const generatedID = `turbo-stream-autofocus-${uuid()}`
+  const turboStreams = fragment.querySelectorAll("turbo-stream")
+  const elementWithAutofocus = firstAutofocusableElementInStreams(turboStreams)
+  let willAutofocusId: string | null = null
+
+  if (elementWithAutofocus) {
+    if (elementWithAutofocus.id) {
+      willAutofocusId = elementWithAutofocus.id
+    } else {
+      willAutofocusId = generatedID
+    }
+
+    elementWithAutofocus.id = willAutofocusId
+  }
+
+  callback()
+  await nextAnimationFrame()
+
+  const hasNoActiveElement = document.activeElement == null || document.activeElement == document.body
+
+  if (hasNoActiveElement && willAutofocusId) {
+    const elementToAutofocus = document.getElementById(willAutofocusId)
+
+    if (elementIsFocusable(elementToAutofocus)) {
+      elementToAutofocus.focus()
+    }
+    if (elementToAutofocus && elementToAutofocus.id == generatedID) {
+      elementToAutofocus.removeAttribute("id")
+    }
+  }
+}
+
+async function withPreservedFocus(callback: () => void) {
+  const [activeElementBeforeRender, activeElementAfterRender] = await around(callback, () => document.activeElement)
+
+  const restoreFocusTo = activeElementBeforeRender && activeElementBeforeRender.id
+
+  if (restoreFocusTo) {
+    const elementToFocus = document.getElementById(restoreFocusTo)
+
+    if (elementIsFocusable(elementToFocus) && elementToFocus != activeElementAfterRender) {
+      elementToFocus.focus()
+    }
+  }
+}
+
+function firstAutofocusableElementInStreams(nodeListOfStreamElements: NodeList) {
+  for (const streamElement of nodeListOfStreamElements) {
+    const elementWithAutofocus = queryAutofocusableElement((streamElement as StreamElement).templateElement.content)
+
+    if (elementWithAutofocus) return elementWithAutofocus
+  }
+
+  return null
 }
